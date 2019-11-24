adf55ba4af328ac1dd237b225be3abebc4dd128f
Swain Wong <swainwong@SwainWong.local>
2019-11-24 20:05:19 +0800

    [feat] remove element

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -10,21 +10,30 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    let i = 0 // 外循环指针
-    let j = 0 // 内循环指针
-    for(; i<nums.length; i++) {
-        // 找到顺序的第一个
-        if (nums[i] === val) {
-           for(j= j>i ? j+1 : i+1; j<nums.length; j++) {
-               // 找到第一个不等于目标值的数
-               if (nums[j] !== val) {
-                    // 后者覆盖前者
-                    nums[i] = nums[j]
-               }
-           }
+function removeElement (arr, val) {
+    const len = arr.length
+	let i = 0 // 慢指针下标
+    let j = 0 // 快指针下标
+    let count = 0 // 统计出现了几次目标元素
+    // 比较愚蠢的办法
+    arr.forEach(item => {if (item === val) {
+       count ++
+     } })
+    // 寻找目标元素
+    for (; i < len; i++) {
+      // 寻找到目标元素
+	  if (arr[i] === val) {
+        // 快指针从慢指针的下一位置开始
+		for (j = i + 1 ; j < len; j++) {
+           // 找到一个不等于目标值的元素
+           if (arr[j] !== val) {
+			// 快慢指针位置元素，进行置换
+            [arr[i],arr[j]] = [arr[j],arr[i]]
+          }
         }
-    }
-};
+      }
+   }
+  arr.length = len - count
+  return arr
+}
 // @lc code=end
-
