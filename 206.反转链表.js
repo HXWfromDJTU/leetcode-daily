7259d5606489b05858a67ef761ec88ee6d7421a2
swainwong <404813471@qq.com>
2020-07-28 01:17:52 +0800

    feat: reverse linked list recursion resolve

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -1,24 +1,8 @@
-/*
- * @lc app=leetcode.cn id=206 lang=javascript
- *
- * [206] 反转链表
- */
-
-// @lc code=start
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(1)
  */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
-// 反转链表的核心操作是，当亲
-var reverseList = function(head) {
+var reverseLi = function(head) {
     // 当前指针和前驱指针，分别指向头结点和null
     let [cur, prev] = [head, null]
     while (cur) {
@@ -29,8 +13,31 @@ var reverseList = function(head) {
          */
         [cur.next, prev, cur] = [prev, cur, cur.next]
     }
-    // 直到最后 cur 指向为空
+    // 直到最后 cur 指向为空，当前的pre自然就是第一个节点
     return prev
 };
-// @lc code=end
+
+
+// 使用 递归的形式
+/**
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(n)
+ */
+var reverseList2 = function(head) {
+    if (head === null) return null // 原本就是个空链表，那么就返回一个空链表
+    if (head.next === null) return head // head.next === null 说明链表只有一个节点
+
+    // 使用问题规模缩小的思维，假设除了头结点外，剩余节点已经反转完了(这里就用反转函数来处理)
+    const newHead = reverseList2(head.next)  // 处理完的节点有一个新的头结点 newHead
+
+    // 处理当前头结点 与 已完成反转的链表之间的关系
+    // 当前情况下是  head --> (A) -->  (B) <--- (C)  <-- (D) <-- (E) <-- (F) <-- newHead
+    head.next.next = head // head.next.next 表示将 (B) 的 next 指向 (A)
+    head.next = null // 然后将 (A) 的 next 指向 null
+    // 结果就是 null <--- (A) <--- (B) <--- (C)  <-- (D) <-- (E) <-- (F) <-- newHead
+    //  head ↗
+
+    // 顺利反转后，返回 newHead
+    return newHead
+};
 
