5e95447bfb3a4245cf22912eca15f646792d7aca
swain wong <swainwong93@gmail.com>
2020-08-12 14:11:07 +0800

    feat: 169.majority (via map)🐭

--- 169.求众数.ts
+++ 169.求众数.ts
@@ -14,22 +14,22 @@
  * Testcase Example:  '[3,2,3]'
  *
  * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
- * 
+ *
  * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1:
- * 
+ *
  * 输入: [3,2,3]
  * 输出: 3
- * 
+ *
  * 示例 2:
- * 
+ *
  * 输入: [2,2,1,1,1,2,2]
  * 输出: 2
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -43,11 +43,16 @@ function majorityElement(nums: number[]): number {
         }
     })
     let maxTimes = 0
-    Object.keys(recorder).forEach(times => {
-        if (Number(times) > maxTimes) {
-            maxTimes = Number(times)
+    let majorityNum = 0
+
+    Object.keys(recorder).forEach((number) => {
+        if (recorder[number] > maxTimes) {
+            maxTimes = recorder[number]
+            majorityNum = Number(number)
         }
     })
+
+    return majorityNum
 };
 // @lc code=end
 
